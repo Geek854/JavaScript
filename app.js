0a342f030dcf8413cce83fe34192058413d2e5e9
geek854 <geekbrains854@gmail.com>
2021-03-29 20:34:52 +0300

    solved tasks of the 5th lesson

--- app.js
+++ app.js
@@ -1,197 +1,134 @@
-lesson2
+// lesson5
 "use strict";
 
-
-// // ПРИМЕР 1
-
-// // 1 Объясните почему код даёт именно такие результаты?
-// //пример 1
-let a = 1,
-    b = 1,
-    c, d;
-c = ++a;
-alert(c); // ответ: 2, потому что инкремент увеличел значение операнда да 1;
-//пример 2
-d = b++;
-alert(d); //ответ: 1, постфиксная форма, значене операнда увеличется после выполнения каманды;
-//пример 3
-c = 2 + ++a;
-alert(c); //ответ: 5, так как а уже равно 2 , операнд увеличит а на 1, то значение а станет 3. 2+3=5;
-//пример 4
-d = 2 + b++;
-alert(d); //ответ: 4, постфиксная форма, значене операнда увеличется после выполнения каманды, но так как предидущие действия в примере 2 с помощью постфиксной формы увеличили операнд на еденицу и он стал равен 2, то соответственно 2+2=4.
-alert(a); //3, так как после примера 3, ни коких действий с переменно а не было значение осталось равным 3.
-alert(b); //3, так как в римере 4, использовалась постфиксная форма, значение операнда увеличилось на 1, соответствено переменная b стала равна 3.
+// 1 поиск элементов на странице
 
 
-// // ПРИМЕР 2
 
-// // 2 Чему будут равны переменные x и a в примере ниже? Написать почему так произошло
-// // (описать последовательность действий).
-let a = 2;
-let x = 1 + (a *= 2);
-// ответ 5, так как () имеют самый высокий приоритет, то сначала производим вычисление в скобках, а*=2, это тоже самое, что а = а*2 или а = 2*2, соответственно переменна а = 4, отсюда следует что х = 1+4, х = 5;
+// <!-- Задание. Используя поиск по id, получите ссылку с текстом link4
+// и выведите ее в консоль. -->
+let link = document.getElementById('four');
+console.log(link);
 
+// <!-- Задание. Используя поиск по имени класса, получите все элементы с классом .one
+//     и выведите их в консоль. -->
+let ones = document.getElementsByClassName('one');
+console.log(ones);
 
-// // ПРИМЕР 3
+// <!-- Задание. Используя поиск по селектору (метод querySelectorAll) найдите только те ссылки с
+// классом.card - link, которые находятся в.card - body
+// и выведите их в консоль. -->
+let links = document.querySelectorAll('.card-body .card-link');
+console.log(links);
 
-// // 3 Объявить две переменные a и b и задать им целочисленные произвольные начальные
-// // значения.
-// // Затем написать скрипт, который работает по следующему принципу:
-// //     -если a и b положительные, вывести их разность(ноль можно считать положительным числом);
-// //     - если а и b отрицательные, вывести их произведение;
-// // - * (этот пункт по сложнее, делайте по желанию) если а и b разных знаков, вывести их сумму;
-let a = +prompt("Введите первое число: ");
-let b = +prompt("Введите второе число: ");;
-let c = 0;
-if (a >= 0 && b >= 0) {
-    c = a - b;
-} else if (a < 0 && b < 0) {
-    c = a * b;
-} else {
-    c = a + b;
-}
-alert(c);
+// <!-- Задание. Используя поиск по селектору (метод querySelector) найдите div с атрибутом
+// aria - valuenow = "50" и выведите его в консоль. -->
+let div = document.querySelector('[aria-valuenow="50"]');
+console.log(div);
+
+// <!-- Задание. Выведите содержимое тега title в консоль. -->
+console.log(document.title);
 
 
-// ПРИМЕР 4
-
-// 4 Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с двумя
-// параметрами. Т.е. например, функция для сложения должна принимать два числа, складывать их
-// и возвращать результат.
-// Обязательно использовать оператор return.
-
-1.
-
-function addition(a, b) {
-    let c = 0;
-    c = a + b;
-    return c;
-}
-let a = +prompt("введите первое число: ");
-let b = +prompt("введите второе число: ");
-let q = addition(a, b);
-alert("Сумма чисел равна: " + q);
-
-2.
-
-function subtract(a, b) {
-    let c = 0;
-    let d = 10;
-    c = a - b - d;
-    return c;
-}
-let a = +prompt("введите первое число: ");
-let b = +prompt("введите второе число: ");
-let q = subtract(a, b);
-alert(q);
-
-3.
-
-function splitUp(a) {
-    let c = 0;
-    c = a / 2;
-    return c;
-}
-let a = +prompt("введите число которое вы хотите поделить пополам: ");
-let q = splitUp(a);
-alert("Половина вашего числа равна " + q);
-
-4.
-
-function multiply(r) {
-    let p = 0;
-    p = 4 * 3.14 * radius();
-
-    function radius() {
-        let g = 0;
-        g = r * r;
-        return g;
-    }
-    return p;
-}
-let r = +prompt("чтобы узнать площадь планеты, укажите ее экваториальный радиус, км: ");
-let q = multiply(r);
-q = parseInt(q).toLocaleString();
-let planet = 0;
-if (r === 2440) {
-    planet = "Меркурий";
-} else if (r === 6052) {
-    planet = "Венера";
-} else if (r === 6378) {
-    planet = "Земля";
-} else if (r === 3397) {
-    planet = "Марс";
-} else if (r === 71490) {
-    planet = "Юпитер";
-} else if (r === 60270) {
-    planet = "Сатурн";
-} else if (r === 25560) {
-    planet = "Уран";
-} else if (r === 24760) {
-    planet = "Нептун";
-} else if (r === 1151) {
-    planet = "Плутон";
-} else {
-    planet = "нам неизвестна"
-}
-alert("Площедь планеты равна " + q + " км.," + " это планета " + planet);
 
 
-// ПРИМЕР 5
+// 2 класс node
 
-// 5 Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation),
-// где arg1, arg2 – значения аргументов, operation – строка с названием операции. В зависимости от
-// переданного значения операции (использовать switch) выполнить одну из арифметических
-// операций
-// (использовать функции из задания 4) и вернуть полученное значение.
-function mathOperation(arg1, arg2, operation) {
-    switch (operation) {
-        case "+":
-            let fold = arg1 + arg2;
-            alert("Результат равен: " + fold);
-            break;
-        case "-":
-            let subtract = arg1 - arg2;
-            alert("Результат равен: " + subtract);
-            break;
-        case "*":
-            let multiply = arg1 * arg2;
-            alert("Результат равен: " + multiply);
-            break;
-        case "/":
-            let splitUp = arg1 / arg2;
-            alert("Результат равен: " + splitUp);
-            break;
-    }
-}
-let arg1 = parseInt(+prompt("введите первое число: "));
-let arg2 = parseInt(+prompt("введите второе число: "));
-let operation = prompt("Что вы хотите сделать с введенными числами? Введите нужный символ ' + '; ' - '; ' * '; ' / '");
-mathOperation(arg1, arg2, operation);
 
-lesson1
-"use strict";
-let Tc = +prompt("Введите температуру в цельсиях");
-let Tf = (9 / 5) * Tc + 32;
-alert("Температура в фарингейтах составит " + Tf);
 
-let name = "Василий";
-let admin = name;
-alert(admin);
 
-let result = 10 + 10 + "10"; // сначала складывается первое и второе число получаем 20 потом присваеваем "10", так как "10" это строка все выражение становится сторокой по этому происходит конкатенация, результат 2010.
-console.log(result);
+// <!-- Задание. Обратитесь к элементу с классом .mt-0 и получите его родителя - элемент с
+// классом.media - body. -->
+let mt0El = document.querySelector('.mt-0');
+console.log(mt0El.parentNode);
 
-let result1 = 10 + "10" + 10; // так как сложение просходит со строкой то вместо сложения происходит конкатенация, результат 101010
-console.log(result1);
+// <!-- Задание. Добавьте в div c помощью appendChild следующую разметку:
+//     <h3>hello world</h3> -->
+let span = document.querySelector('span'); /*div заменил на  span во избежаниии конфликтов*/
+let h3 = document.createElement('h3');
+h3.innerText = 'hello world';
+span.appendChild(h3);
 
-let result2 = 10 + 10 + +"10"; // результат будет 30, так как использован унарный + который привел строку "10" к числу 10.
-console.log(result2);
+// <!-- Задание. 
+//     1. Найдите элемент с классом .btn-outline-warning и удалите его.
+//     2. Аналогичным образом удалите элемент с классом .btn-outline-dark -->
+let btnWarn = document.querySelector('.btn-outline-warning');
+btnWarn.remove();
+let btnDark = document.querySelector('.btn-outline-dark');
+btnDark.remove();
 
-let result3 = 10 / -""; // в "" значение числовое отрицательное NaN, поэтому результат, - infiniti
-console.log(result3);
 
-let result4 = 10 / +"2,5"; // результат равен NaN, так как в JS разделение идет (.), поэтому js не видет числа и не может поделить число на строку, а так подазревает что результатом должно быть число выдает нам NaN
-console.log(result4);
-main
+
+// 3 работа с атрибутами
+
+
+
+// <!-- Задание. Найдите кнопку с классом .btn-info и сделайте текст в кнопке: "Кнопка".-->
+let btn = document.querySelector('.btn-info');
+btn.innerText = 'Кнопка';
+
+// <!-- Задание. Найдите тег ul. И добавьте ему класс one. -->
+let ul = document.querySelector('ul');
+ul.classList.toggle('one');
+console.log(ul);
+
+// <!-- Задание. Найдите элемент с классом .nav-link и используя метод toggle из свойства classList
+// у элемента, добавьте или уберите класс.active. -->
+let ul1 = document.querySelector('.nav-link');
+ul1.classList.toggle('active');
+console.log(ul1);
+
+// <!-- Задание. Найдите элемент с классом .dropdown-menu и удалите этот класс, используя свойство
+// classList у элемента, и метод remove(). -->
+let menu = document.querySelector('.dropdown-menu');
+menu.classList.remove('dropdown-menu');
+console.log(menu);
+
+// <!-- Задание. Выведите в консоль значение атрибутов data-dismiss
+// и aria - label с помощью метода getAttribute. -->
+let button = document.querySelector('button');
+console.log(button.getAttribute('data-dismiss'));
+console.log(button.getAttribute('aria-label'));
+
+// <!-- Задание. Добавьте заголовку атрибут data-one="один" и атрибут
+// data - two = "два" с помощью метода setAttribute. -->
+let h1 = document.querySelector('h1');
+h1.setAttribute('data-one', 'один');
+h1.setAttribute('data-two', 'два');
+console.log(h1);
+
+// <!-- Задание. найдите заголовок с id="title" и замените значение id
+// на что - то другое. -->
+let heading = document.getElementById('title');
+heading.id = 'title1';
+console.log(heading);
+
+
+// 4 обработка событий
+
+
+// <!-- Задание. Отследите когда все теги будут добавлены на страницу (событие
+// DOMContentLoaded) и выведите alert 'ом сообщение: "все теги добавлены на страницу". -->
+document.addEventListener('DOMContentLoaded', function() {
+    alert("все теги добавлены на страницу");
+});
+
+// <!-- Задание. Дождитесь когда все картинки загрузятся (событие load) и
+//     покажите с помощью alert сообщение: "все картинки загружены". -->
+window.addEventListener('load', function() {
+    alert("все картинки загружены");
+});
+
+// <!-- Задание. Сделайте, чтобы по клику по кнопке с классом .btn появлялся
+// alert('клик') -->
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', function() {
+    alert('клик');
+});
+
+// <!-- Задание. Сделайте, чтобы при наведении на textarea в консоли
+// появлялось сообщение: "вы навели на textarea" -->
+let textarea = document.querySelector('textarea');
+textarea.addEventListener('mouseover', function() {
+    console.log("вы навели на textarea");
+});
